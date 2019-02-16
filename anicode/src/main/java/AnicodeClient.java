f7e21b075beafae48f5d0ea8813523b6a8ba4bb1
dexmon <dexmon@example.com>
2019-02-16 18:07:47 +0900

    feat: 不要なimportを削除する

--- anicode/src/main/java/AnicodeClient.java
+++ anicode/src/main/java/AnicodeClient.java
@@ -1,12 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.regex.Pattern;
 
 /** Anicode2.4 -Unicorn-
