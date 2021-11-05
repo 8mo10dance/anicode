d670b74552093623a383bc9e4a6abf7a1cb9c681
8mo10dance <8mo10dance@protonmail.com>
2021-11-05 21:38:11 +0900

    もともとあった Anime クラスを削除する

--- anicode/src/main/java/entity/Anime.java
+++ anicode/src/main/java/entity/Anime.java
@@ -1,6 +1,11 @@
 package entity;
 
+import java.io.File;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Anime {
   public final String name;
@@ -11,5 +16,42 @@ public class Anime {
     this.name = name;
     this.episodeList = episodeList;
     this.historyList = historyList;
+  }
+
+  public Optional<History> getLatestHistory() {
+    return historyList.stream().max(Comparator.comparing(h -> h.createdAt));
+  }
+
+  public Optional<File> getAnimeFilePath(int ep) {
+    if (containsEpisode(ep)) {
+      return Optional.of(episodeList.stream().map(e -> e.file).collect(Collectors.toList()).get(ep - 1));
+    }
+
+    return Optional.empty();
+  }
+
+  public Date getUpdatedAt() {
+    var defaultHistoryCreatedAt=  new Date();
+    defaultHistoryCreatedAt.setTime(0);
+
+    return getLatestHistory().map(h -> h.createdAt).orElse(defaultHistoryCreatedAt);
+  }
+
+  public Optional<Integer> getNextEpisode() {
+    var currentEp = getLatestHistory().map(h -> h.ep).orElse(0);
+    var nextEp = currentEp + 1;
+    if (containsEpisode(nextEp)) {
+      return Optional.of(nextEp);
+    }
+
+    return Optional.empty();
+  }
+
+  public boolean isOnGoing() {
+    return getNextEpisode().isPresent();
+  }
+
+  private boolean containsEpisode(int ep) {
+    return ep > 0 && ep <= episodeList.size();
   }
 }
